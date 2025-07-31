0b2741b09205db304751605098cea72c74a34ebc
akhyar00 <149314626+akhyar00@users.noreply.github.com>
2025-07-31 06:53:30 +0000

    Final check on all config files

--- bootstrap/vercel.php
+++ bootstrap/vercel.php
@@ -1,12 +1,7 @@
 <?php
-
 // File: bootstrap/vercel.php
-
 use Illuminate\Foundation\Application;
-
 $app = require __DIR__.'/app.php';
-
 // Beritahu Laravel untuk menggunakan folder /tmp untuk semua penyimpanan file.
 $app->useStoragePath('/tmp/storage');
-
 return $app;
