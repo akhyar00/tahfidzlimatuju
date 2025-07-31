c6da33e5e13b7f45bbfe112d6a3ea688a688161b
akhyar00 <149314626+akhyar00@users.noreply.github.com>
2025-07-31 01:07:42 +0000

    Adopt standard Vercel PHP structure

--- api/index.php
+++ api/index.php
@@ -1,6 +1,1 @@
-<?php
-
-// File: api/index.php
-
-// Cukup arahkan ke file index.php asli di dalam folder public
-require __DIR__.'/../public/index.php';
+<?php require __DIR__.'/../public/index.php'; ?>
